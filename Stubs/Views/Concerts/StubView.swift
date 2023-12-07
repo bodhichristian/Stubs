c0cd6df2045ba26552b770d92e160418c8446cd6
bodhichristian <clavelle23@gmail.com>
2023-12-07 17:18:56 -0500

    Update large size stub to scale in a GeometryReader. Update font style.

--- Stubs/Views/Concerts/StubView.swift
+++ Stubs/Views/Concerts/StubView.swift
@@ -29,11 +29,11 @@ struct StubView: View {
     @State private var iconTapped = false // For icon animation
     
     var body: some View {
-        ZStack(alignment: .topLeading) {
+        ZStack(alignment: .center) {
             ticketStubBase
             concertDetails
         }
-        .padding(.horizontal)
+        //.padding(.horizontal)
     }
 }
 
@@ -44,67 +44,72 @@ struct StubView: View {
 extension StubView {
     // Create stub base, color, and icon
     private var ticketStubBase: some View {
-        ZStack(alignment: .bottomTrailing) {
-            RoundedRectangle(cornerRadius: 20) // Stub Background
-                .foregroundStyle(Color(colorName: concert.accentColor)!)
-                .frame(
-                    width: size == .small ? 200 : 400,
-                    height: size == .small ? 100 : 280)
-                .shadow(radius: 6, y: 10)
-                .padding()
-            
-            RoundedRectangle(cornerRadius: 20) // Gradient overlay
-                .foregroundStyle(gradient)
-                .frame(
-                    width: size == .small ? 200 : 400,
-                    height: size == .small ? 100 : 280)
-                .padding()
-            
-            Image(systemName: concert.iconName) // Concert icon
-                .resizable()
-                .scaledToFit()
-                .frame(
-                    width: size == .small ? 30 : 80,
-                    height: size == .small ? 30 : 80)
-                .foregroundStyle(.white)
-                .shadow(radius: 4, y: 6)
-                .padding(size == .small ? 30 : 40)
-                .symbolEffect(.bounce, options: .nonRepeating, value: iconTapped)
-                .onAppear {
-                    iconTapped.toggle()
-                }
-                .onTapGesture {
-                    iconTapped.toggle()
-                }
+        GeometryReader { geo in
+            ZStack(alignment: .bottomTrailing) {
+                RoundedRectangle(cornerRadius: 20) // Stub Background
+                    .foregroundStyle(Color(colorName: concert.accentColor)!)
+                    .frame(
+                        width: size == .small ? 200 : geo.size.width * 0.9,
+                        height: size == .small ? 100 : geo.size.width * 0.63)
+                    .shadow(radius: 6, y: 10)
+                
+                RoundedRectangle(cornerRadius: 20) // Gradient overlay
+                    .foregroundStyle(gradient)
+                    .frame(
+                        width: size == .small ? 200 : geo.size.width * 0.9,
+                        height: size == .small ? 100 : geo.size.width * 0.63)
+                
+                Image(systemName: concert.iconName) // Concert icon
+                    .resizable()
+                    .scaledToFit()
+                    .frame(
+                        width: size == .small ? 30 : geo.size.width * 0.2,
+                        height: size == .small ? 30 : geo.size.width * 0.2)
+                    .foregroundStyle(.white)
+                    .shadow(radius: 4, x: 2, y: 6)
+                    .padding(size == .small ? 30 : 25)
+                    .symbolEffect(.bounce, options: .nonRepeating, value: iconTapped)
+                    .onAppear {
+                        iconTapped.toggle()
+                    }
+                    .onTapGesture {
+                        iconTapped.toggle()
+                    }
+            }
+            .position(x: geo.size.width / 2, y: geo.size.width / 2)
         }
     }
     // Display concert details
     private var concertDetails: some View {
-        VStack(alignment: .leading) {
-            Text(artistName)
-                .font(titleFont)
-                .foregroundStyle(.white)
-                .shadow(radius: 2)
-                
+        GeometryReader { geo in
             
-            if size == .large {
-                Text(concert.venue)
+            VStack(alignment: .leading) {
+                Spacer()
+                Text(artistName)
+                    .font(titleFont)
+                    .foregroundStyle(.white)
+                    .shadow(radius: 2)
+                    
+                
+                if size == .large {
+                    Text(concert.venue)
+                        .font(secondaryFont)
+                        .foregroundStyle(.black)
+                    
+                    Text(concert.city)
+                        .font(secondaryFont)
+                        .foregroundStyle(.white)
+                }
+                
+                Spacer()
+                
+                Text(concert.date.formatted(date: .abbreviated, time: .omitted)) // Format: Jun 9, 2023
                     .font(secondaryFont)
                     .foregroundStyle(.black)
-                
-                Text(concert.city)
-                    .font(secondaryFont)
-                    .foregroundStyle(.white)
             }
-            
-            Spacer()
-            
-            Text(concert.date.formatted(date: .abbreviated, time: .omitted)) // Format: Jun 9, 2023
-                .font(secondaryFont)
-                .foregroundStyle(.white)
+            .frame(height: size == .small ? 70 : geo.size.height * 0.7)
+            .padding(size == .small ? 30 : 40)
         }
-        .frame(height: size == .small ? 70 : 210)
-        .padding(size == .small ? 30 : 40)
     }
     // Create a display-ready artist name, truncate where needed
     private var artistName: String {
@@ -135,7 +140,7 @@ extension StubView {
         case .small:
             return .subheadline.bold()
         default:
-            return .title2.bold()
+            return .title2
         }
     }
 }
