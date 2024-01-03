dc4bd7be7de10e97b3b0ac842670f7e8476dd56e
bodhichristian <clavelle23@gmail.com>
2024-01-02 22:24:41 -0500

    Move brief details to an overlay on the banner image in ArtistDetailView.

--- Stubs/Views/ArtistDetailView/ArtistDetailView.swift
+++ Stubs/Views/ArtistDetailView/ArtistDetailView.swift
@@ -19,9 +19,9 @@ struct ArtistDetailView: View {
     @Environment(\.dismiss) var dismiss
     
     @Query var concerts: [Concert]
-
+    
     @StateObject var viewModel = ArtistDetailView.ViewModel()
-
+    
     
     private var filteredConcerts: [Concert] {
         return concerts.filter({$0.artist == artist })
@@ -32,104 +32,125 @@ struct ArtistDetailView: View {
     @State private var imageOpacity = 0.0
     
     var body: some View {
+        
         GeometryReader { geo in
+            
             ZStack {
-
-                VStack {
+                
+                VStack { // banner layer
                     
-                    ZStack {
+                    ZStack(alignment: .bottomTrailing) { // banner image
                         
-                        Rectangle()
+                        Rectangle() // base layer
                         
+                        // if the artist search has received a response
+                        // create `artist` object using the first item in the array
                         if let artist = viewModel.artists.first {
-                            AsyncImage(url: URL(string: artist.strArtistFanart2 ?? "")) { backgroundImage in
-                                backgroundImage
+                            AsyncImage(
+                                url: URL(
+                                    string: artist.strArtistFanart2 ?? ""
+                                )
+                            ) { bannerImage in
+                                bannerImage
                                     .resizable()
                                     .scaledToFill()
                                     .frame(width: geo.size.width, height: geo.size.width / 2)
                                     .clipShape(Rectangle())
                                     .opacity(imageOpacity)
                                     .overlay {
-                                        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+                                        LinearGradient(colors: [.clear, .black], startPoint: .top, endPoint: .bottom)
                                             .opacity(0.6)
                                     }
-
+                                
                             } placeholder: {
                                 
                                 Rectangle()
-
+                                
                             }
-
+                            
+                            
+                            VStack(alignment: .trailing) { // Genre & Location
+                                
+                                Text(artist.strGenre ?? "")
+                                    .font(.title3)
+                                    .fontWeight(.semibold)
+                                Text(artist.strCountry ?? "")
+                                
+                            }
+                            .foregroundStyle(.white)
+                            .padding()
                         }
                         
                         
+                        
+                         
                     }
                     .frame(maxWidth: geo.size.width, maxHeight: geo.size.width / 2)
                     
                     Spacer()
                 }
                 
-                VStack(spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {
                     
-                    ZStack {
-                        
-                        Circle()
-                            .foregroundStyle(.gray)
-                            .frame(width: artistImageWidth)
-                            .padding()
+                    HStack {
+                        ZStack {
                             
-                        if let artist = viewModel.artists.first {
-                            AsyncImage(url: URL(string: artist.strArtistThumb ?? "")) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: artistImageWidth)
-                                    .clipShape(Circle())
-                                    .opacity(imageOpacity)
-                                    .shadow(radius: 7,  y: 7)
-                                    .padding()
-                                    .onAppear {
-                                        withAnimation(.easeInOut(duration: 0.7)){
-                                            imageOpacity = 1.0
+                            Circle()
+                                .foregroundStyle(.gray)
+                                .frame(width: artistImageWidth)
+                                .padding()
+                            
+                            if let artist = viewModel.artists.first {
+                                AsyncImage(url: URL(string: artist.strArtistThumb ?? "")) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: artistImageWidth)
+                                        .clipShape(Circle())
+                                        .opacity(imageOpacity)
+                                        .shadow(radius: 7,  y: 7)
+                                        .padding()
+                                        .onAppear {
+                                            withAnimation(.easeInOut(duration: 0.7)){
+                                                imageOpacity = 1.0
+                                            }
                                         }
-                                    }
                                     
-                            } placeholder: {
+                                } placeholder: {
+                                    
+                                    Circle()
+                                        .foregroundStyle(.gray)
+                                        .frame(width: artistImageWidth)
+                                        .padding()
+                                    
+                                }
                                 
-                                Circle()
-                                    .foregroundStyle(.gray)
-                                    .frame(width: artistImageWidth)
-                                    .padding()
-
                             }
-                                                        
+                            
                         }
-
+                        
+                        
+                        
                     }
+                    
                     .padding(.top, geo.size.width / 5)
                     
-
                     
-                    if let artist = viewModel.artists.first {
-                        HStack(spacing: 0) {
-                            Text(artist.strGenre ?? "")
-                            Text(" | ")
-                            Text(artist.strCountry ?? "")
-                                .bold()
-                        }
-                    } else {
-                        
-                        Text( " " )
-                    }
- 
+                    
+                    
                     ScrollView {
-
+                        
+                        if let artist = viewModel.artists.first {
+                            Text(artist.strBiographyEN ?? "")
+                        }
+                        
                         ArtistDetailVenuesMap(concerts: filteredConcerts)
                         
                         AlbumsScrollView(artist: artist)
                     }
                 }
-                   
+                
+
             }
             .navigationTitle(artist)
             .onAppear {
