6e92a846c8b162b7c499a0e5f685dc1e32879d82
bodhichristian <clavelle23@gmail.com>
2024-01-24 13:22:33 -0500

    Continue building ProfileView components.

--- Stubs/Views/Profile/ProfileView.swift
+++ Stubs/Views/Profile/ProfileView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ProfileView: View {
     @Environment(\.colorScheme) var colorScheme
+    
+    @State private var imageSelection: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
@@ -29,8 +32,36 @@ struct ProfileView: View {
                         // Veritcal Lines
                         HStack {
                             VerticalLineBoundary()
-                            
+                            VStack(alignment: .leading) {
+                                Text("Username")
+                                    .font(.largeTitle)
+                                    .fontWeight(.semibold)
+                                
+                                Text("User since Jan 2024")
+                                    .foregroundStyle(.secondary)
+                                Text("Favorite Venue")
+                                
+                                Spacer()
+                            }
                             Spacer()
+                            
+                            VStack {
+                                Spacer()
+                                Circle()
+                                    .frame(width: geo.size.width / 3)
+                                    .overlay(alignment: .bottomTrailing) {
+                                            PhotosPicker(selection: $imageSelection,
+                                                         matching: .images,
+                                                         photoLibrary: .shared()) {
+                                                Image(systemName: "pencil.circle.fill")
+                                                    .symbolRenderingMode(.multicolor)
+                                                    .font(.system(size: 30))
+                                                    .foregroundColor(.accentColor)
+                                            }
+                                            .buttonStyle(.borderless)
+                                        }
+                                
+                            }
                             
                             VerticalLineBoundary()
                         }
