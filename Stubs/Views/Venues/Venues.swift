2b47a6a4dad3ae3f0ab349b66aabb1b644817ecb
bodhichristian <clavelle23@gmail.com>
2024-01-27 13:43:22 -0500

    Continue building Venues tab

--- Stubs/Views/Venues/Venues.swift
+++ Stubs/Views/Venues/Venues.swift
@@ -12,17 +12,49 @@ import SwiftData
 struct Venues: View {
     @Query var concerts: [Concert]
     
+    @State private var location: MKMapItem?
+    @State private var position: MapCameraPosition = .automatic
+    
     var body: some View {
         NavigationStack {
             GeometryReader { geo in
-                Map {
+                Map(position: $position, selection: $location) {
                     ForEach(concerts, id: \.uuid) { concert in
                         
-                        Marker(concert.venue, coordinate: CLLocationCoordinate2D(latitude: concert.venueLatitude, longitude: concert.venueLongitude))
+                        
+                        Marker(
+                            concert.venue,
+                            coordinate: CLLocationCoordinate2D(
+                                latitude: concert.venueLatitude,
+                                longitude: concert.venueLongitude
+                            )
+                        )
+                        
+                        
+                            
                     }
                 }
                 .padding(.bottom, 60)
-//
+                .onAppear {
+                    location = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: concerts.first?.venueLatitude ?? 0.0, longitude: concerts.first?.venueLongitude ?? 0.0)))
+                    
+                    position = .camera(
+                        MapCamera(
+                            centerCoordinate: CLLocationCoordinate2D(
+                                latitude: concerts.first?.venueLatitude ?? 0.0,
+                                longitude: concerts.first?.venueLongitude ?? 0.0
+                            ),
+                            
+                            distance: 12000000,
+                            heading: 100,
+                            pitch: 60)
+
+                    )
+                    
+                    
+                    
+                    
+                }
             }
             .navigationTitle("Venues")
             .toolbarBackground(.hidden, for: .navigationBar)
