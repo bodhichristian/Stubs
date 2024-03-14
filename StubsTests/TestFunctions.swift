4318a9e105c99ec6c7c60bbac3dce139459020bf
bodhichristian <clavelle23@gmail.com>
2024-03-14 12:18:31 -0400

    Update TestFunctions.randomString()

--- StubsTests/TestFunctions.swift
+++ StubsTests/TestFunctions.swift
@@ -10,8 +10,19 @@ import Foundation
 struct TestFunctions{
     // Generate a random string from 0 to 25 characters in length
     func randomString() -> String {
-        let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,./?!@#$%^&*()_-+=;:'\""
+        let characters =
+        """
+        abcdefghijklmnopqrstuvwxyz
+        ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+        ,./?!@#$%^&*()_-+=;:'\"
+        """
         let randomLength = Int.random(in: 0...25)
-        return String((0..<randomLength).map { _ in characters.randomElement()! })
+        let randomString = String(
+            (0..<randomLength)
+                .map {_ in
+                    characters.randomElement()!
+        }
+        )
+        return randomString
     }
 }
