d1e285ca044feaa868770751e295aba086cb5092
bodhichristian <clavelle23@gmail.com>
2024-03-21 16:20:06 -0400

    Tidy up.

--- StubsTests/StubEditor-ViewModelTests.swift
+++ StubsTests/StubEditor-ViewModelTests.swift
@@ -9,15 +9,7 @@ import XCTest
 import SwiftData
 @testable import Stubs
 
-final class StubEditorViewModelTests: XCTestCase {
-    
-    func testStubEditorViewModelInit() {
-        let context: ModelContext
-        
-        
-        let viewModel = StubEditor.ViewModel()
-        
-    }
-
-
-}
+//final class StubEditorViewModelTests: XCTestCase {
+//    
+//
+//}
