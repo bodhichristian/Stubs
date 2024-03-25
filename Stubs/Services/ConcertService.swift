7840a1459604ce0232c2168773207137dc6e06bd
bodhichristian <clavelle23@gmail.com>
2024-03-25 14:10:58 -0400

    Update ConcertService.swift to eliminate unecessary do-catch blocks.

--- Stubs/Services/ConcertService.swift
+++ Stubs/Services/ConcertService.swift
@@ -12,42 +12,30 @@ import SwiftUI
 class ConcertService {
     let artistService = ArtistService()
     let mapKitService = MapKitService()
-
+    
     var template = Concert(
         iconName: StubStyle.icons.randomElement()!,
         accentColor: StubStyle.colors.randomElement()!
     )
     
     func buildConcert(with artist: Artist? = nil) async throws {
-        do {
-            try await fetchArtist(artist)
-            try await getVenueCoordinates()
-        } catch {
-            throw error
-        }
+        try await fetchArtist(artist)
+        try await getVenueCoordinates()
     }
     
     private func fetchArtist(_ artist: Artist?) async throws {
         if let savedArtist = artist {
             template.artist = savedArtist
         } else {
-            do {
-                try await artistService.search(for: template.artistName)
-                template.artist = artistService.fetchedArtist
-            } catch {
-                throw ArtistServiceError.failedToFetchArtist
-            }
+            try await artistService.search(for: template.artistName)
+            template.artist = artistService.fetchedArtist
         }
     }
     
     private func getVenueCoordinates() async throws {
-        do {
-            let coordinates = try await mapKitService.getCoordinates(for: template)
-            template.venueLatitude = coordinates.0
-            template.venueLatitude = coordinates.1
-        } catch {
-            throw error
-        }
+        let coordinates = try await mapKitService.getCoordinates(for: template)
+        template.venueLatitude = coordinates.0
+        template.venueLatitude = coordinates.1
     }
 }
 
