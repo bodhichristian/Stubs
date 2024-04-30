3d3fe4797a27f8234809245f7e3af4d9c6fa0cd9
bodhichristian <clavelle23@gmail.com>
2024-04-29 23:04:47 -0400

    GitHub Actions Tests

---
+++ Package.swift
@@ -0,0 +1,26 @@
+import PackageDescription
+
+let package = Package(
+    name: "Stubs",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "Stubs",
+            targets: ["Stubs", "StubsTests", "StubsUITests"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Stubs",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "StubsTests",
+            dependencies: ["Stubs"],
+            path: "Tests"),
+    ]
+    
+)
